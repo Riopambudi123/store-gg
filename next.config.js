5f283b97508254aea84910cc65974b0715a0c82c
support <fyan.mo8@gmail.com>
2022-09-21 11:01:28 +0700

    store-gg

--- next.config.js
+++ next.config.js
@@ -2,6 +2,12 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
+};
 
-module.exports = nextConfig
+module.exports = {
+  images: {
+    domains: ["api-bwa-storegg.herokuapp.com"],
+  },
+};
+
+module.exports = nextConfig;
